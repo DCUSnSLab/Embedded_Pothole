7697a665c868c4506c069a2520e94c959558f7c5
sujong <dgdgksj@gmail.com>
2023-12-03 01:49:35 +0900

    feat: confidence score and timestampe to csv
    기존 스크립트에 탐지되는 객체의 신뢰도와 시간 등을 csv로 실시간 저장
    기능 추가
    
    향후 이미지 저장 및 gps 위치 추가 등을 고려하여 쓰레드로 동작하게 함
    
    추가 개발 및 리팩토링 예정

--- run_demo.py
+++ run_demo.py
@@ -10,19 +10,45 @@ import os
 import shutil
 import jetson_utils
 import jetson_inference
+import threading
+import queue
+import csv
+from datetime import date
+def export_to_excel(q):
+    now = time.localtime()
+    while True:
+        if not q.empty():
+            data = q.get()
+            with open('detections.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                # writer.writerows(str(float(data.Confidence)))
+                # writer.writerows("%04d/%02d/%02d %02d:%02d:%02d" % (
+	            #     now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
+                writer.writerow([str(float(data.Confidence)),"%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)])
+                # print(str(float(data.Confidence)))
+
+
 
 # net = jetson_inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-net = jetson_inference.detectNet(argv=["--model=/home/snslab/po/jetson-inference/data/networks/mycustom/ssd-mobilenet.onnx", "--labels=/home/snslab/po/jetson-inference/data/networks/mycustom/classes.txt","--input-blob=input_0", "--output-cvg=scores", "--output-bbox=boxes"], threshold=0.5)
+net = jetson_inference.detectNet(argv=["--model=/home/snslab/po/jetson-inference/data/networks/pothole_v1/ssd-mobilenet.onnx", "--labels=/home/snslab/po/jetson-inference/data/networks/pothole_v1/classes.txt","--input-blob=input_0", "--output-cvg=scores", "--output-bbox=boxes"], threshold=0.5)
 # net = jetson_inference.segNet("fcn-resnet18-cityscapes-1024x512")
 # net = jetson_inference.detectNet("data/networks/mycustom/ssd-mobilenet.onnx", threshold=0.5)
 # net = jetson_inference.detectNet("/home/snslab/po/jetson-inference//python/training/detection/ssd/models/ssd-inception-v2/ssd-mobilenet.onnx", threshold=0.5)
 camera = jetson_utils.gstCamera(1280,720,'/dev/video4')
 # load the object detection model
+data_queue = queue.Queue()
 
+excel_thread = threading.Thread(target=export_to_excel, args=(data_queue,))
+excel_thread.start()
 display = jetson_utils.glDisplay()
 
 while display.IsOpen():
 	img,width, height = camera.CaptureRGBA()
 	detections = net.Detect(img,width,height)
+	for data in detections:
+		for data in detections:
+			data_queue.put(data)
+			# exit()
 	display.RenderOnce(img,width,height)
 	display.SetTitle("dddd")
+
